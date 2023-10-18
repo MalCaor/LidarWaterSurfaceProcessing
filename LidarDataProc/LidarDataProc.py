21493c30d365b83ebabc9ee89e33cb4525647b25
MalCaor <xav.lemen@orange.fr>
2023-10-18 16:24:17 +0200

    get gyro data

--- LidarDataProc/LidarDataProc.py
+++ LidarDataProc/LidarDataProc.py
@@ -83,15 +83,14 @@ def parse_gyro_file_data(path_file_input: str) -> List[GyroData]:
     array_retour: List[GyroData] = []
 
     # read file
-    with open(path_file_input, mode ='r')as f:
-        csvFile = csv.DictReader(f)
+    with open(path_file_input) as f:
+        csvFile = csv.DictReader(f, strict=True)
  
         # displaying the contents of the CSV file
         i: int = 0
         for row in csvFile:
-            if i != 0:
-                g_data: GyroData = GyroData(list(row))
-                array_retour.append(g_data)
+            g_data: GyroData = GyroData(row)
+            array_retour.append(g_data)
             i += 1
 
     return array_retour
@@ -104,14 +103,14 @@ def write_gyro_data(array_data: List[GyroData], path_file_output: str):
     length: float = len(array_data)
     i: float = 0.0
     # fo through array
-    for point in array_data:
+    for data in array_data:
         # % compl
         print(" "*20, end='\r')
         percent: float = i / length * 100.0
         print("{:.0f}/{} - {:.2f}%".format(i, length, percent), end='\r')
         i += 1
         # write
-        f.write(str(point)+"\n")
+        f.write(str(data)+"\n")
 
     print(" "*20, end='\r')
     print("Writing file {} Finished".format(path_file_output))
