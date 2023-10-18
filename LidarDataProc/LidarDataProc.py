b381e8e87a8addbb99361c2f4fb542561c012a28
MalCaor <xav.lemen@orange.fr>
2023-10-18 10:38:37 +0200

    read file

--- LidarDataProc/LidarDataProc.py
+++ LidarDataProc/LidarDataProc.py
@@ -11,29 +11,50 @@ from LidarPoint import LidarPoint
 """point data 'names': ['x', 'y', 'z', 'intensity', 'ring', 'time']"""
 
 # Function
-def parse_file_data(path_file_input: str, path_file_output: str):
+def parse_file_data(path_file_input: str,) -> List[LidarPoint]:
     print("PARSING FILE : {}".format(path_file_input))
+
     # test if input
     if not exists(path_file_input):
         raise FileNotFoundError("Input file doesn't exist")
+
     # config
     config = vd.Config(model='VLP-16', rpm=300)
     pcap_file = path_file_input
     cloud_arrays: List[LidarPoint] = []
 
+    # get data length
+    dataLidar = vd.read_pcap(pcap_file, config)
+    length: float = sum(1 for _ in dataLidar)
+
     # read file
+    i: float = 0.0
     for stamp, points in vd.read_pcap(pcap_file, config):
+        # % compl
+        print(" "*20, end='\r')
+        percent: float = i / length * 100.0
+        print("{:.0f}/{} - {:.2f}%".format(i, length, percent), end='\r')
+        i += 1
+        # get points
         for point in points:
             lidarpoint: LidarPoint = LidarPoint(point)
             cloud_arrays.append(lidarpoint)
+    
+    return cloud_arrays
+    
 
+def write_array_point(cloud_arrays: List[LidarPoint], path_file_output: str):
     # write output
+    print("Writing output in {}".format(path_file_output))
     f = open(path_file_output, "w")
+    # mesure length
+    length: float = len(cloud_arrays)
+    i: float = 0.0
+    # fo through array
     for point in cloud_arrays:
-        f.write(str("\n ---------\n"))
-        f.write(str(point))
-        f.write(str("\n ---------\n"))
+        f.write(str(point)+"\n")
     f.close()
+
 
 # argument parsing
 parser = argparse.ArgumentParser(
@@ -51,4 +72,5 @@ parser.add_argument(
 args = parser.parse_args()
 
 if args.lidar:
-    parse_file_data(args.lidar[0], args.lidar[1])
+    array = parse_file_data(args.lidar[0])
+    write_array_point(array, args.lidar[0])
