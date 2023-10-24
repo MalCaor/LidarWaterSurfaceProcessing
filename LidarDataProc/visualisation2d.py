488f6a3fce38e13fc67119e6a31cddc4764ef036
MalCaor <xav.lemen@orange.fr>
2023-10-24 11:48:55 +0200

    start 2d anim

--- LidarDataProc/visualisation2d.py
+++ LidarDataProc/visualisation2d.py
@@ -4,6 +4,7 @@ from LidarPointArray import LidarPointArray
 import pandas as pd
 
 from matplotlib import pyplot as plt
+from matplotlib import animation as anim
 import seaborn
 
 def display2DcloudPoint(array_cloud: List[LidarPoint]):
@@ -41,4 +42,14 @@ def contour2dcloudPoint(array_cloud: List[LidarPoint]):
     plt.tricontourf(x, y, z, cmap="ocean")
     plt.xlabel('x coordinates')
     plt.ylabel('y coordinates')
+    plt.show()
+
+def contour2dAnimates(array_cloud: List[LidarPointArray]):
+    print("join plot")
+    plt.tricontourf(
+        [p[0] for p in array_cloud[0].points_array],
+        [p[1] for p in array_cloud[0].points_array],
+        [p[2] for p in array_cloud[0].points_array]
+    )
+    print("display plot")
     plt.show()
