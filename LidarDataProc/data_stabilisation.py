79e06e3432798278b32d1e5989ed0945e9171a96
MalCaor <xav.lemen@orange.fr>
2023-10-27 14:06:47 +0200

    simplify

--- LidarDataProc/data_stabilisation.py
+++ LidarDataProc/data_stabilisation.py
@@ -5,6 +5,15 @@ from typing import List
 from LidarPointArray import LidarPointArray
 from GyroData import GyroData
 
+
+def _correct_array_point(array_lidar, tot_accel_x, tot_accel_y, tot_accel_z):
+    lid = array_lidar
+    for y in range(len(lid.points_array)):
+        lid.points_array[y][0] = lid.points_array[y][0] * tot_accel_x
+        lid.points_array[y][1] = lid.points_array[y][1] * tot_accel_y
+        lid.points_array[y][2] = lid.points_array[y][2] * tot_accel_z
+    return lid
+
 def stabilise_lidar_array(array_lidar: List[LidarPointArray], array_gyro: List[GyroData]):
     print("Stabilising data of lenght : {}".format(len(array_lidar)))
     # array of corrected points
@@ -33,26 +42,18 @@ def stabilise_lidar_array(array_lidar: List[LidarPointArray], array_gyro: List[G
             break
         while(i<len(array_lidar) and array_lidar[i].timestamp < gyr.timestamp):
             # data to correct
-            lid = array_lidar[i]
-            for y in range(len(lid.points_array)):
-                lid.points_array[y][0] = lid.points_array[y][0] * -1 * tot_accel_x
-                lid.points_array[y][1] = lid.points_array[y][1] * -1 * tot_accel_y
-                lid.points_array[y][2] = lid.points_array[y][2] * -1 * tot_accel_z
+            lid = _correct_array_point(array_lidar[i], tot_accel_x, tot_accel_y, tot_accel_z)
             new_array.append(lid)
             i += 1
         # correct tot gyr
-        tot_accel_x += float(gyr.accel_x)
-        tot_accel_y += float(gyr.accel_y)
-        tot_accel_z += float(gyr.accel_z)
+        tot_accel_x += float(gyr.gyro_x)
+        tot_accel_y += float(gyr.gyro_y)
+        tot_accel_z += float(gyr.gyro_z)
 
     # if some lidar data left... use last ditch correction
     while(i<len(array_lidar)):
         # data to correct
-        lid = array_lidar[i]
-        for y in range(len(lid.points_array)):
-            lid.points_array[y][0] = lid.points_array[y][0] * tot_accel_x
-            lid.points_array[y][1] = lid.points_array[y][1] * tot_accel_y
-            lid.points_array[y][2] = lid.points_array[y][2] * tot_accel_z
+        lid = _correct_array_point(array_lidar[i], tot_accel_x, tot_accel_y, tot_accel_z)
         new_array.append(lid)
         i += 1
 
