3084893cce9073d58a5ca2ab72c9b0c13d2621f0
MalCaor <xav.lemen@orange.fr>
2023-12-12 11:08:10 +0100

    import filter

--- LidarDataProc/data_filter.py
+++ LidarDataProc/data_filter.py
@@ -1,5 +1,6 @@
 from typing import List
 from LidarPointArray import LidarPointArray
+from filter import *
 
 def filter_lidar_data(lidar_data: List[LidarPointArray], filter_setting_path: str) -> List[LidarPointArray]:
     return lidar_data
