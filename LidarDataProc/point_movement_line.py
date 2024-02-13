59ed4efadee523262915e1288da23cb34d9af892
MalCaor <xav.lemen@orange.fr>
2024-02-13 10:58:05 +0100

    calculate wave cluster timeapse

--- LidarDataProc/point_movement_line.py
+++ LidarDataProc/point_movement_line.py
@@ -7,11 +7,10 @@ from scipy import stats
 
 def wave_cluster_timesapse_generator(wave_clusters_frames):
     length: float = len(wave_clusters_frames)
-    print("Interpreting array of length {}".format(str(len(wave_clusters_frames))))
-
     list_wave_cluster_timelase_retour: List[WaveClusterTimelapse] = []
     
     i = 0
+    print("linking clusters waves")
     for frame in wave_clusters_frames:
         # display
         print(" "*10, end='\r')
@@ -36,7 +35,24 @@ def wave_cluster_timesapse_generator(wave_clusters_frames):
         # append frame
         for lone_point in frame:
             list_wave_cluster_timelase_retour.append(WaveClusterTimelapse([lone_point]))
-    
+    print("linking clusters waves Finished!")
+
+    # remove useless timelapse
+    list_wave_cluster_timelase_retour = [timelapse for timelapse in list_wave_cluster_timelase_retour if len(timelapse.wave_snapshots)>2]
+
+    i = 0
+    length = len(list_wave_cluster_timelase_retour)
+    print("calculating line regression")
+    timelapse: WaveClusterTimelapse
+    for timelapse in list_wave_cluster_timelase_retour:
+        # display
+        print(" "*10, end='\r')
+        percent: float = i / length * 100.0
+        print("{:.0f}/{} - {:.2f}%".format(i, length, percent), end='\r')
+        i+=1
+        timelapse.lin_regr()
+    print("line regression Finished!")
+
     print("wave_cluster_timesapse_generator finished")
     return list_wave_cluster_timelase_retour
 
