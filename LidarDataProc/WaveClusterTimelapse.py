a15cc53af5db285b57d7cd64f4443d0ba0f94762
MalCaor <xav.lemen@orange.fr>
2024-02-14 10:35:33 +0100

    360

--- LidarDataProc/WaveClusterTimelapse.py
+++ LidarDataProc/WaveClusterTimelapse.py
@@ -18,3 +18,5 @@ class WaveClusterTimelapse:
         res = stats.linregress(lx, ly)
         self.slope, self.intercept, self.rvalue, self.pvalue = res.slope, res.intercept, res.rvalue, res.pvalue
         self.angle = np.rad2deg(np.arctan2(1 - -1, 1*self.slope - -1*self.slope))
+        if self.wave_snapshots[0].barycentre[0] < self.wave_snapshots[len(self.wave_snapshots)-1].barycentre[0]:
+            self.angle = (self.angle+180)%360 # wave come from behind
