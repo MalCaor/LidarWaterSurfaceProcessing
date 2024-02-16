79eec743f8d930ba41655927c33e771539243759
MalCaor <xav.lemen@orange.fr>
2024-02-16 17:17:47 +0100

    dist

--- LidarDataProc/WaveClusterTimelapse.py
+++ LidarDataProc/WaveClusterTimelapse.py
@@ -3,6 +3,7 @@ from typing import List
 from WaveCluster import WaveCluster
 from scipy import stats
 import numpy as np
+from utils import calculate_distance
 
 class WaveClusterTimelapse:
     def __init__(self, wave_snapshots) -> None:
@@ -10,6 +11,7 @@ class WaveClusterTimelapse:
         self.slope, self.intercept, self.rvalue, self.pvalue = None, None, None, None
         self.angle = None
         self.finished = False
+        self.length_bary = None
 
     def lin_regr(self):
         wave: WaveCluster
@@ -20,3 +22,6 @@ class WaveClusterTimelapse:
         self.angle = np.rad2deg(np.arctan2(1 - -1, 1*self.slope - -1*self.slope))
         if self.wave_snapshots[0].barycentre[0] < self.wave_snapshots[len(self.wave_snapshots)-1].barycentre[0]:
             self.angle = (self.angle+180)%360 # wave come from behind
+        self.length_bary = calculate_distance(
+            np.array(self.wave_snapshots[0].barycentre[0]), 
+            np.array(self.wave_snapshots[len(self.wave_snapshots)-1].barycentre[0]))
