cddbd6e4a372d9059f8da475d6146526b75b5969
MalCaor <xav.lemen@orange.fr>
2024-02-19 11:18:21 +0100

    simulated wave

--- LidarDataProc/SimulatedSea.py
+++ LidarDataProc/SimulatedSea.py
@@ -1,5 +1,6 @@
 
 import datetime
+from random import random
 from typing import List
 from LidarPointArray import LidarPointArray
 import numpy as np
@@ -14,8 +15,28 @@ class SimulatedSea:
     def get_array_lidar(self):
         array_retour: List[LidarPointArray] = []
         for i in range(self.nbr_frames):
-            pc = np.random.rand(500,3)
+            waves = self._generate_waves_base(5)
+            for wave in waves:
+                self._move_points(wave, i, [0,1,0])
+            pc = np.concatenate(waves)
             stamp: datetime = self.start + datetime.timedelta(0,self.intervals*i)
             frame: LidarPointArray = LidarPointArray(stamp.timestamp(), pc)
             array_retour.append(frame)
         return array_retour
+
+    def _generate_waves_base(self, nbr_waves):
+        array_waves = []
+        for i in range(nbr_waves):
+            pc = np.random.rand(200,3)
+            rand = (random()-0.5)*2
+            self._move_points(pc, i, [rand*5,rand*1,0])
+            array_waves.append(pc)
+        return array_waves
+
+    def _move_points(self, pc, i_frame, xyz):
+        for i in range(len(pc)):
+            point = pc[i]
+            point[0] = point[0]+(i_frame*xyz[0])
+            point[1] = point[1]+(i_frame*xyz[1])
+            point[2] = point[2]+(i_frame*xyz[2])
+            pc[i] = point
