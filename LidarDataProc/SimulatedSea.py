0524f685f8f48585d7c4e8b794fef1264dae04c5
MalCaor <xav.lemen@orange.fr>
2024-02-19 11:40:00 +0100

    better rand

--- LidarDataProc/SimulatedSea.py
+++ LidarDataProc/SimulatedSea.py
@@ -28,17 +28,20 @@ class SimulatedSea:
 
     def _generate_waves_base(self, nbr_waves):
         array_waves = []
-        for i in range(nbr_waves):
+        for _ in range(nbr_waves):
             pc = np.random.rand(200,3)
-            rand = (random()-0.5)*2
-            self._move_points(pc, i, [rand*5,rand*5,0])
+            randx = (random()-0.5)*2
+            randy = (random()-0.5)*2
+            randz = (random()-0.5)*2
+            self._move_points(pc, 1, [randx*5,randy*5,randz])
             array_waves.append(pc)
         return array_waves
 
     def _move_points(self, pc, i_frame, xyz):
         for i in range(len(pc)):
+            rand = (random()-0.5)*2
             point = pc[i]
-            point[0] = point[0]+(i_frame*xyz[0])
-            point[1] = point[1]+(i_frame*xyz[1])
-            point[2] = point[2]+(i_frame*xyz[2])
+            point[0] = point[0]+(rand+i_frame*xyz[0])
+            point[1] = point[1]+(rand+i_frame*xyz[1])
+            point[2] = point[2]+(rand+i_frame*xyz[2])
             pc[i] = point
