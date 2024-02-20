a5bddcfa2cb180149eabd7039e9c7d38979643ec
MalCaor <xav.lemen@orange.fr>
2024-02-20 16:01:54 +0100

    polar

--- LidarDataProc/visualisationStat.py
+++ LidarDataProc/visualisationStat.py
@@ -1,3 +1,4 @@
+import math
 from statistics import mean, median
 from matplotlib import pyplot as plt
 from matplotlib import animation as anim
@@ -6,6 +7,56 @@ from utils import moving_average
 
 from WaveClusterTimelapse import WaveClusterTimelapse
 from WaveCluster import WaveCluster
+
+def polar_angle(timestamps, timeslapses):
+    plt.axes(polar=True)
+    # lines angles
+    angleslines = []
+    for timeslapse in timeslapses:
+        line_time = []
+        line_angle = []
+        for wave in timeslapse.wave_snapshots:
+            line_time.append(wave.timestamp)
+            line_angle.append(timeslapse.angle)
+        angleslines.append([line_time, line_angle, timeslapse.rvalue, timeslapse.length_bary])
+    for line in angleslines:
+        alpha = abs(line[2])/2 + (min(line[3],5)/5)/2
+        plt.plot([math.radians(angle) for angle in line[1]], line[0], color="black", alpha=alpha)
+    
+    # moy angles
+    moy = []
+    med = []
+    pondered_moy = []
+    pondered_med = []
+    for timestamp in timestamps:
+        concerned_timeslapses = []
+
+        timeslapse: WaveClusterTimelapse
+        for timeslapse in timeslapses:
+            wave_snap: WaveCluster
+            for wave_snap in timeslapse.wave_snapshots:
+                if wave_snap.timestamp == timestamp:
+                    concerned_timeslapses.append(timeslapse)
+        if concerned_timeslapses:
+            # moy / med
+            moy.append(mean([timeslapse.angle for timeslapse in concerned_timeslapses]))
+            med.append(median([timeslapse.angle for timeslapse in concerned_timeslapses]))
+            # pondered weight
+            tot_angle = []
+            for timeslapse in concerned_timeslapses:
+                weight = abs(timeslapse.rvalue)*5 + min(timeslapse.length_bary,5)
+                weight = int(weight)
+                for _ in range(weight):
+                    tot_angle.append(timeslapse.angle)
+            pondered_moy.append(mean(tot_angle))
+            pondered_med.append(median(tot_angle))
+    #plt.plot(timestamps, moy, label="mean")
+    #plt.plot(timestamps, med, label="median")
+    #plt.plot(timestamps, pondered_moy, label="pondered mean")
+    #plt.plot(pondered_med, timestamps, label="pondered median")
+    plt.plot([math.radians(angle) for angle in moving_average(pondered_med, 20)], timestamps, label="average pondered median")
+    plt.legend(loc='best')
+    plt.show()
 
 def stat_angle(timestamps, timeslapses):
     # lines angles
