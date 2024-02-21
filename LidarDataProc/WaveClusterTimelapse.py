aa1e7f16eb6f06e9a4c7eb0605dad314323fa96e
MalCaor <xav.lemen@orange.fr>
2024-02-21 11:02:23 +0100

    better but still not better

--- LidarDataProc/WaveClusterTimelapse.py
+++ LidarDataProc/WaveClusterTimelapse.py
@@ -19,8 +19,16 @@ class WaveClusterTimelapse:
         res = stats.linregress(lx, ly)
         self.slope, self.intercept, self.rvalue, self.pvalue = res.slope, res.intercept, res.rvalue, res.pvalue
         self.angle = np.rad2deg(np.arctan2(1 - -1, 1*self.slope - -1*self.slope))
-        if self.wave_snapshots[0].barycentre[0] < self.wave_snapshots[len(self.wave_snapshots)-1].barycentre[0]:
-            self.angle = (self.angle+180)%360 # wave come from behind
+        # test if angle is on the other side of the compase
+        if self.angle < 45:
+            if self.wave_snapshots[0].barycentre[1] < self.wave_snapshots[len(self.wave_snapshots)-1].barycentre[1]:
+                self.angle = (self.angle+180)%360 # wave come from behind
+        elif self.angle < 135:
+            if self.wave_snapshots[0].barycentre[0] < self.wave_snapshots[len(self.wave_snapshots)-1].barycentre[0]:
+                self.angle = (self.angle+180)%360 # wave come from behind
+        else:
+            if self.wave_snapshots[0].barycentre[1] > self.wave_snapshots[len(self.wave_snapshots)-1].barycentre[1]:
+                self.angle = (self.angle+180)%360 # wave come from behind
         self.length_bary = calculate_distance(
             np.array(self.wave_snapshots[0].barycentre[0]), 
             np.array(self.wave_snapshots[len(self.wave_snapshots)-1].barycentre[0]))
