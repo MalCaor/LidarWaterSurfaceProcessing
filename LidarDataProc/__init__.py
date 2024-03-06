6227bc2d594698a144b54b23b59cc07832ebf6b0
MalCaor <xav.lemen@orange.fr>
2024-03-06 16:57:08 +0100

    beter desc

--- LidarDataProc/__init__.py
+++ LidarDataProc/__init__.py
@@ -1,1 +1,14 @@
-# LIDAR PROCESSING DATA
+"""
+LIDAR WATER SURFACE PROCESSING :
+
+This Python Script allow to Process a LIDAR film scan of a water body's surface.
+
+It can then display various informations about it, mainly the waves directions.
+
+If used as a module you can implement your own algorithm by using the various functions, mainly:
+
+- file_parser: to parse a .pcap LIDAR file be it from a ouster or velodyne lidar.
+- data_stabilisation: to stabilise the Lidar Point array with IMU data.
+- line_generator: mainly for wave_clustering that enable to KNN the point cloud
+- point_movement_line: process a clusterised array to follow the waves movement
+"""
