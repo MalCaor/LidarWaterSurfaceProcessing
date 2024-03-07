2de8a74dd5bae2fc4dfb8f99eb2083f96ba50b35
MalCaor <xav.lemen@orange.fr>
2024-03-07 14:15:17 +0100

    write data

--- LidarDataProc/write_data.py
+++ LidarDataProc/write_data.py
@@ -3,6 +3,12 @@ from typing import List
 from GyroData import GyroData
 
 def write_gyro_data(array_data: List[GyroData], path_file_output: str):
+    """Legacy function to write GyroData in a file
+
+    Args:
+        array_data (List[GyroData]): GyroData array
+        path_file_output (str): path of the file to write the data in (created if doesn't exist)
+    """
     # write output
     print("Writing output in {}".format(path_file_output))
     f = open(path_file_output, "w")
